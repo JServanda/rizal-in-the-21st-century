151aebca8e9abedaad2a4ba3efa8714300b9d79c
JServanda <jaservanda@up.edu.ph>
2025-06-02 10:13:44 +0800

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rizal-in-the-21st-century/',
+  base: '/rizal-21st-century/',
   plugins: [react()]
 })
